f8230c9d6b381ee5f03dbd325d29b32c83227de0
Vishal Kulkarni <vishalkulkarni@Vishals-MacBook-Pro.local>
2023-05-08 22:11:47 -0400

    Initial Commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract Inbox{
+    string public message;
+
+    function Inbox(string intitialMessage) public{
+        message = intitialMessage;
+    }
+
+    function setMessage(string newMessage) public{
+        message = newMessage;
+    }
+
+}
